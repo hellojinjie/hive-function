883a8c3d9520cce3ff113dfbcd535a9d923bf153
hellojinjie <hellojinjie@gmail.com>
2013-10-29 15:48:35 +0800

    rename package

---
+++ src/main/java/shaman/hive/udf/GenericUDAFCdnBytesLoaded.java
@@ -0,0 +1,144 @@
+package shaman.hive.udf;
+
+import java.util.Map;
+
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.parse.SemanticException;
+import org.apache.hadoop.hive.ql.udf.generic.AbstractGenericUDAFResolver;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDAFEvaluator;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDAFEvaluator.AggregationBuffer;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDAFParameterInfo;
+import org.apache.hadoop.hive.serde2.objectinspector.MapObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorUtils;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorUtils.ObjectInspectorCopyOption;
+import org.apache.hadoop.hive.serde2.typeinfo.TypeInfo;
+
+import com.google.common.collect.Maps;
+
+@Description(value="_FUNC_ calculate the cdn bytes loaded", name="cdnbytesloaded")
+public class GenericUDAFCdnBytesLoaded extends AbstractGenericUDAFResolver {
+
+    @Override
+    public GenericUDAFEvaluator getEvaluator(TypeInfo[] parameters)
+            throws SemanticException {
+        throw new SemanticException(
+              "This UDAF does not support the deprecated getEvaluator() method.");
+    }
+
+    @Override
+    public GenericUDAFEvaluator getEvaluator(GenericUDAFParameterInfo info)
+            throws SemanticException {
+        ObjectInspector[] ois = info.getParameterObjectInspectors();
+        if (ois.length != 1) {
+            throw new UDFArgumentLengthException("_FUNC_ take only one parameter");
+        }
+        return new GenericUDAFCdnBytesLoadedEvaluator();
+    }
+
+    public static class CdnAggregationBuffer implements AggregationBuffer {
+        Map<String, Long> bytesLoaded;
+    }
+    
+    public static class GenericUDAFCdnBytesLoadedEvaluator extends GenericUDAFEvaluator {
+        
+        private MapObjectInspector inputOI;
+        private MapObjectInspector outputOI;
+        private Map<String, String> cdnPatterns;
+        
+        {
+            cdnPatterns = Maps.newHashMap();
+            cdnPatterns.put("cdnl3nl", "Level3");
+            cdnPatterns.put("cdnak", "Akamai");
+            cdnPatterns.put("cndllnwnl", "LimeLight");
+            cdnPatterns.put("cdncd", "CDNetworks");
+        }
+        
+        @Override
+        public ObjectInspector init(Mode m, ObjectInspector[] parameters) throws HiveException {
+            super.init(m, parameters);
+            if (parameters.length != 1) {
+                throw new UDFArgumentLengthException("In GenericUDAFCdnBytesLoadedEvaluator "
+                        + "parameters length is not 1");
+            }
+            inputOI = (MapObjectInspector) parameters[0];
+            outputOI = (MapObjectInspector) ObjectInspectorUtils
+                    .getStandardObjectInspector(inputOI, ObjectInspectorCopyOption.JAVA);
+            return outputOI;
+        }
+        
+        @Override
+        public AggregationBuffer getNewAggregationBuffer() throws HiveException {
+            CdnAggregationBuffer buffer = new CdnAggregationBuffer();
+            buffer.bytesLoaded = Maps.newHashMap();
+            return buffer;
+        }
+
+        @Override
+        public void reset(AggregationBuffer agg) throws HiveException {
+            CdnAggregationBuffer buffer = (CdnAggregationBuffer) agg;
+            buffer.bytesLoaded.clear();
+        }
+
+        @Override
+        public void iterate(AggregationBuffer agg, Object[] parameters)
+                throws HiveException {
+            if (parameters.length == 1 && parameters[0] != null) {
+                merge(agg, parameters[0]);
+            }
+        }
+
+        @Override
+        public Object terminatePartial(AggregationBuffer agg) throws HiveException {
+            return ((CdnAggregationBuffer) agg).bytesLoaded;
+        }
+
+        @Override
+        public void merge(AggregationBuffer agg, Object partial) throws HiveException {
+            if (partial == null) {
+                return;
+            }
+            try {
+                CdnAggregationBuffer buffer = (CdnAggregationBuffer) agg;
+                @SuppressWarnings("unchecked")
+                Map<String, Long> bytesLoaded = (Map<String, Long>) inputOI.getMap(partial);
+                for (String key : bytesLoaded.keySet()) {
+                    boolean matched = false;
+                    for (String pattern : cdnPatterns.keySet()) {
+                        if (key.contains(pattern)) {
+                            String cdnName = cdnPatterns.get(pattern);
+                            Long loaded = buffer.bytesLoaded.get(cdnName);
+                            if (loaded == null) {
+                                loaded = bytesLoaded.get(key);
+                            } else {
+                                loaded = loaded > bytesLoaded.get(key) ? loaded : bytesLoaded.get(key);
+                            }
+                            buffer.bytesLoaded.put(cdnName, loaded);
+                            matched = true;
+                            break;
+                        }
+                    }
+                    if (matched == false) {
+                        Long loaded = buffer.bytesLoaded.get(key);
+                        if (loaded == null) {
+                            loaded = bytesLoaded.get(key);
+                        } else {
+                            loaded = loaded > bytesLoaded.get(key) ? loaded : bytesLoaded.get(key);
+                        }
+                        buffer.bytesLoaded.put(key, loaded);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public Object terminate(AggregationBuffer agg) throws HiveException {
+            return ((CdnAggregationBuffer) agg).bytesLoaded;
+        }
+        
+    }
+}
---
+++ src/main/java/shaman/hive/udf/UDFDetermineAppType.java
@@ -0,0 +1,52 @@
+package shaman.hive.udf;
+
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.JavaStringObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+
+@Description(value="_FUNC_ get appType from deviceType and appType. "
+        + "Some message miss appType field, we need to determine the appType from deviceType", 
+        name="determineAppType")
+public class UDFDetermineAppType extends GenericUDF {
+
+    private JavaStringObjectInspector stringInspector;
+    private AppTypeMapping appTypeMapping;
+    
+    @Override
+    public Object evaluate(DeferredObject[] arg0) throws HiveException {
+        if (arg0.length != 2) {
+            throw new UDFArgumentLengthException("_FUNC_ take two arguments, "
+                    + "first is the deviceType, second is the appType");
+        }
+        String deviceType = stringInspector.getPrimitiveJavaObject(arg0[0].get());
+        String appType = stringInspector.getPrimitiveJavaObject(arg0[1].get());
+        if ("".equals(appType)) {
+            appType = appTypeMapping.get(deviceType);
+            if (appType == null) {
+                return "";
+            } else {
+                return appType;
+            }
+        } else {
+            return appType;
+        }
+    }
+
+    @Override
+    public String getDisplayString(String[] arg0) {
+        return "Determine App Type";
+    }
+
+    @Override
+    public ObjectInspector initialize(ObjectInspector[] arg0) throws UDFArgumentException {
+        this.appTypeMapping = AppTypeMapping.getInstance();
+        this.stringInspector = PrimitiveObjectInspectorFactory.javaStringObjectInspector;
+        return PrimitiveObjectInspectorFactory.javaStringObjectInspector;
+    }
+    
+}
