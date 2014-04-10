28f3b57da042cb9fe5671048f7fe8b7caaba542c
hellojinjie <hellojinjie@gmail.com>
2014-04-10 16:02:50 +0800

    add debug info

--- src/main/java/shaman/hive/udf/UDFMapValueSum.java
+++ src/main/java/shaman/hive/udf/UDFMapValueSum.java
@@ -9,6 +9,7 @@ import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
 import org.apache.hadoop.hive.serde2.objectinspector.MapObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
 
 @Description(value="_FUNC_ sum values of a map", 
@@ -16,6 +17,7 @@ import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectIn
 public class UDFMapValueSum extends GenericUDF {
 
     private MapObjectInspector inputOI;
+    private ObjectInspector valueOI;
     
     @Override
     public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
@@ -23,16 +25,21 @@ public class UDFMapValueSum extends GenericUDF {
             throw new UDFArgumentLengthException("_FUNC_ take one arguments, map");
         }
         this.inputOI = (MapObjectInspector) arguments[0];
+        valueOI = inputOI.getMapValueObjectInspector();
         return PrimitiveObjectInspectorFactory.javaLongObjectInspector;
     }
 
     @Override
     public Object evaluate(DeferredObject[] arguments) throws HiveException {
-        long result = 0;
-        @SuppressWarnings("unchecked")
-        Map<String, Long> map = (Map<String, Long>) inputOI.getMap(arguments[0].get());
-        for (Long l : map.values()) {
-            result += l;
+        
+        long result = 0l;
+        Map<?, ?> map = inputOI.getMap(arguments[0].get());
+        for (Object l : map.values()) {
+            valueOI.getCategory();
+            PrimitiveObjectInspector oi = PrimitiveObjectInspectorFactory.getPrimitiveObjectInspectorFromClass(valueOI.getClass());
+            System.out.println(oi);
+            System.out.println(valueOI.getCategory());
+            System.out.println(valueOI.getTypeName());
         }
         return result;
     }
